e00a6953af39dda861fac1ced4e983e8d657ac22
Edward John <edjohn@Edwards-MacBook-Air-4.local>
2024-01-24 15:32:34 -0800

    Complete Phases 1,2,3

--- sshell.c
+++ sshell.c
@@ -4,6 +4,80 @@
 #include <unistd.h>
 
 #define CMDLINE_MAX 512
+#define ARG_MAX 16
+#define PATH_MAX 1024
+
+struct Command {
+        char *args[ARG_MAX];
+        int numArgs;
+};
+
+void changeDir(struct Command *command) {
+        int errorCode = chdir(command->args[1]);
+        if (errorCode != 0) {
+                perror("cd failed\n");
+        }
+}
+
+void printWorkingDir() {
+        char currentWorkingDir[PATH_MAX];
+        char* output = getcwd(currentWorkingDir, sizeof(currentWorkingDir));
+        if (output != NULL) {
+                printf("%s\n", output);
+        }
+}
+
+int executeBuiltInCommand(struct Command* command) {
+        if (strcmp(command->args[0], "cd") == 0) {
+                changeDir(command);
+                return 0;
+        }
+        else if (strcmp(command->args[0], "pwd") == 0) {
+                printWorkingDir();
+                return 0;
+        }
+        else {
+                return 1;
+        }
+}
+
+void execute(struct Command *command) {
+        if (executeBuiltInCommand(command) == 0) {
+                return;
+        }
+        pid_t pid;
+        pid = fork();
+        // Execute the command if we are the child process
+        if (pid == 0) {
+                execvp(command->args[0],command->args);
+                exit(1);
+        }
+        // Print our completion message if we are the parent process
+        else if (pid > 0) {
+                int status;
+                waitpid(pid, &status, 0);
+                fprintf(stderr, "Completed '%s' [%d]\n", command->args[0], WEXITSTATUS(status));
+        }
+        // Simply exit if there was a forking error 
+        else {
+                exit(1);
+        }
+}
+
+void parseInput(char *cmd, struct Command *command) {
+        int argCount = 0;
+        char *token = strtok(cmd, " ");
+        if (token != NULL) {
+                command -> args[argCount] = token;
+        }
+        while (token != NULL) {
+                token = strtok(NULL, " ");
+                argCount++;
+                command->args[argCount] = token;
+        }
+        command->numArgs = argCount;
+}
+
 
 int main(void)
 {
@@ -11,7 +85,6 @@ int main(void)
 
         while (1) {
                 char *nl;
-                int retval;
 
                 /* Print prompt */
                 printf("sshell$ ");
@@ -31,16 +104,17 @@ int main(void)
                 if (nl)
                         *nl = '\0';
 
-                /* Builtin command */
+                /* Handle exit */
                 if (!strcmp(cmd, "exit")) {
+                        fprintf(stderr, "Completed '%s' [%d]\n", cmd, 0);
                         fprintf(stderr, "Bye...\n");
                         break;
                 }
 
                 /* Regular command */
-                retval = system(cmd);
-                fprintf(stdout, "Return status value for '%s': %d\n",
-                        cmd, retval);
+                struct Command command;
+                parseInput(cmd, &command);
+                execute(&command);
         }
 
         return EXIT_SUCCESS;
